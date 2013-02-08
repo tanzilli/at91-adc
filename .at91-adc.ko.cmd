f3997296681736c42cc9817b058ae23545a42ac2
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-02-08 17:43:58 +0100

    Clean up

--- .at91-adc.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/tanzilli/at91-adc/driver/at91-adc.ko := arm-linux-gnueabi-ld -EL -r  -T /home/tanzilli/linux/foxg20-linux-2.6.38/scripts/module-common.lds --build-id  -o /home/tanzilli/at91-adc/driver/at91-adc.ko /home/tanzilli/at91-adc/driver/at91-adc.o /home/tanzilli/at91-adc/driver/at91-adc.mod.o
--- .tmp_versions/at91-adc.mod
+++
@@ -1,2 +0,0 @@
-/home/tanzilli/ariag25/at91-adc/at91-adc.ko
-/home/tanzilli/ariag25/at91-adc/at91-adc.o
